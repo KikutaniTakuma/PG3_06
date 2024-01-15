87fe03e7df1a46a71ce8817fe77179851ed3bf7f
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-01-15 10:18:19 +0900

    変数名変更

--- main.cpp
+++ main.cpp
@@ -5,25 +5,25 @@
 #include <mutex>
 
 int main() {
-	std::mutex mtx_;
-	std::thread thread1{ [&mtx_]() {
-		std::lock_guard<std::mutex> lock_{mtx_};
+	std::mutex mtx;
+	std::thread thread1{ [&mtx]() {
+		std::lock_guard<std::mutex> lock{mtx};
 		std::cout << "thread1" << std::endl;
 		} };
+	thread1.join();
 
-	std::thread thread2{ [&mtx_]() {
-		std::lock_guard<std::mutex> lock_{mtx_};
+	std::thread thread2{ [&mtx]() {
+		std::lock_guard<std::mutex> lock_{mtx};
 		std::cout << "thread2" << std::endl;
 		} };
+	thread2.join();
 
-	std::thread thread3{ [&mtx_]() {
-		std::lock_guard<std::mutex> lock_{mtx_};
+	std::thread thread3{ [&mtx]() {
+		std::lock_guard<std::mutex> lock_{mtx};
 		std::cout << "thread3" << std::endl;
 		} };
 
 
-	thread1.join();
-	thread2.join();
 	thread3.join();
 
 	return 0;
